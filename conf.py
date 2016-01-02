79457904822e51fc717ba183fc01892e1773096c
Evgeni Golov <evgeni@golov.de>
2016-01-02 19:12:30 +0100

    do not set base tag, this fucks with same origin polocies

--- conf.py
+++ conf.py
@@ -503,7 +503,7 @@ CREATE_MONTHLY_ARCHIVE = True
 # the browser resolve relative links.
 # In some rare cases, this will be a problem, and you can
 # disable it by setting USE_BASE_TAG to False.
-# USE_BASE_TAG = True
+USE_BASE_TAG = False
 
 # Final location for the blog main RSS feed is:
 # output / TRANSLATION[lang] / RSS_PATH / rss.xml
