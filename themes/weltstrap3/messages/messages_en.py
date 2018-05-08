71883e41959d77f248b4d929b2ce0efcf948b98c
Evgeni Golov <evgeni@golov.de>
2018-05-08 21:59:32 +0200

    start static comment support

---
+++ themes/weltstrap3/messages/messages_en.py
@@ -0,0 +1,7 @@
+MESSAGES = {
+        "No comments.": "No comments.",
+        "{0} wrote on {1}:": "{0} wrote on {1}:",
+        "No comments": "No comments",
+        "{0} comments": "{0} comments",
+        "{0} comment": "{0} comment",
+        }
