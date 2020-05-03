53eca78af4857e69dbb42cf326fedbbc2714c175
Evgeni Golov <evgeni@golov.de>
2020-05-03 11:23:40 +0200

    UNSLUGIFY_TITLES is deprecated

--- conf.py
+++ conf.py
@@ -1290,7 +1290,7 @@ USE_CDN = False
 
 # If you hate "Filenames with Capital Letters and Spaces.md", you should
 # set this to true.
-UNSLUGIFY_TITLES = True
+FILE_METADATA_UNSLUGIFY_TITLES = True
 
 # Additional metadata that is added to a post when creating a new_post
 # ADDITIONAL_METADATA = {}
